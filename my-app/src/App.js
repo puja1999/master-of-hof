df77cba68910c7bfabef9d974cad12c9cc026d8e
Puja <pujasah2501@gmai.com>
2021-05-22 22:54:46 +0530

    completed

---
+++ my-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import './components/HigherOrderComp';
+import HigherOrder from './components/HigherOrderComp';
+
+function App() {
+  return (
+    <div className="App">
+ < HigherOrder/>
+    </div>
+  );
+}
+
+export default App;
